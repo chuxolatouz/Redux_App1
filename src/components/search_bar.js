1fac574ec28729eb2175498e7a206cbc326b90d1
JesusRincon <00jjessejj00@gmail.com>
2016-07-12 15:59:06 -0400

    Added video detail

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,8 +11,7 @@ class SearchBar extends Component {
       <div>
         <input
         value={this.state.term}
-        onChange={event => this.setState({term: event.target.value})} />
-        Value of the input: { this.state.term}
+        onChange={event => this.setState({term: event.target.value})} />        
       </div>
     );
   }
