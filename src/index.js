a62086abce50aeaaee62d8868cef9f4ffcadd9fe
JesusRincon <00jjessejj00@gmail.com>
2016-07-12 13:57:07 -0400

    handler events

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDqdLBVsRZ5liPt-T21f79152UYSm_EGQQ';
 
 const App = () => {
-  return <div> Hi! </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+ );
 }
 
 
